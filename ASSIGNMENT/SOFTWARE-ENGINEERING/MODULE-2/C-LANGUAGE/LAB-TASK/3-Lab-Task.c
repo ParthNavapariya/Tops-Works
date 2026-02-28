c4cd76ecc4a2a8e7e80edae62527cf5cf9a29c9e
Parth Navapariya <145651514+ParthNavapariya@users.noreply.github.com>
2026-02-28 11:52:17 +0530

    Add files via upload

---
+++ ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-2/C-LANGUAGE/LAB-TASK/3-Lab-Task.c
@@ -0,0 +1,17 @@
+/*Write a C program that includes variables, constants, and comments. Declare
+and use different data types (int, char, float) and display their values.*/
+
+
+
+#include<stdio.h>
+int main(){
+    char ch = 'a'; // charachte variabler
+    int  number = 13; // int variable
+    float per = 14.2; // float variable
+
+    //diplay value
+    printf("\nchar value = %c",ch);
+    printf("\nnumber value = %d",number);
+    printf("\nper value = %f",per);
+
+}//end program
